24627987741c4dbc682a604df1d5f84911907074
pd4d10 <pd4d10@gmail.com>
2017-01-06 09:29:01 +0800

    Add navigator icons

--- src/main/nav.js
+++ src/main/nav.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import './nav.css'
 
-const Nav = ({
-  onBack,
-  onForward,
-  onRefresh,
-}) => (
-  <div>
-    <a href="#" onClick={onBack}>back</a>
-    <a href="#" onClick={onForward}>forward</a>
-    <a href="#" onClick={onRefresh}>refresh</a>
-  </div>
-)
+// Icons from https://material.io/icons/
+
+class Nav extends PureComponent {
+  state = {
+    canGoBack: false,
+    canGoForward: false,
+    isRefreshing: false,
+  }
+
+  refresh() {
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <ul>
+          <li onClick={() => history.back()}>
+            <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0h24v24H0z" fill="none"/>
+              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+            </svg>
+          </li>
+          <li onClick={() => history.forward()}>
+          <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/>
+          </svg>
+          </li>
+          <li onClick={this.fresh}>
+            <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/>
+              <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default Nav
