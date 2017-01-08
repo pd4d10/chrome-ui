c663751dddf46bd7c2386bc05e554cdcdecc5d95
pd4d10 <pd4d10@gmail.com>
2017-01-08 14:22:10 +0800

    Use cssnext syntax

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -147,17 +147,17 @@ module.exports = {
     ]
   },
 
-  // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
-      autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9', // React doesn't support IE8 anyway
-        ]
-      }),
+      require('postcss-cssnext'),
+      // autoprefixer({
+      //   browsers: [
+      //     '>1%',
+      //     'last 4 versions',
+      //     'Firefox ESR',
+      //     'not ie < 9', // React doesn't support IE8 anyway
+      //   ]
+      // }),
     ];
   },
   plugins: [
