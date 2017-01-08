decb31ab7fc3e50dbbc97fb6b54381ccfc109fb6
pd4d10 <pd4d10@gmail.com>
2017-01-08 12:10:07 +0800

    Fix tab style

--- src/main/browser.js
+++ src/main/browser.js
@@ -11,8 +11,7 @@ const Browser = ({ tabs, activeTab, addTab, selectTab, removeTab, url }) => (
   <div className="browser">
     <ul className="tabs">
       {tabs.map(({ id, title }) => (
-        <li key={id} onClick={selectTab(id)}>
-          <div className="tab">
+        <li key={id} onClick={selectTab(id)} className="tab">
             <div className="tab-content">{title}</div>
             <svg
               onClick={removeTab(id)}
@@ -20,7 +19,6 @@ const Browser = ({ tabs, activeTab, addTab, selectTab, removeTab, url }) => (
               <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
               <path d="M0 0h24v24H0z" fill="none"/>
             </svg>
-          </div>
         </li>
       ))}
       <li onClick={addTab} className="add"></li>
