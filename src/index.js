62a815626fc0e817b35e2ddf0d7be2a20c18c238
pd4d10 <pd4d10@gmail.com>
2017-01-10 15:34:28 +0800

    Add immutable

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import Immutable from 'seamless-immutable'
 import { v4 } from 'uuid'
 import App from './containers/app'
 import reducers from './reducers'
 
 const id = v4()
 
-const presistedState = {
+const presistedState = Immutable({
   activeTab: id,
   tabs: {
     [id]: {
@@ -17,7 +18,7 @@ const presistedState = {
       title: 'Wikipedia - Wikipedia',
     },
   },
-}
+})
 
 const middlewares = [thunk]
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose // eslint-disable-line
