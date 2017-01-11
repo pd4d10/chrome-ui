eb54bd25e942ed14f68d4e903910b9326881f2c3
pd4d10 <pd4d10@gmail.com>
2017-01-11 18:48:01 +0800

    Add GitHub Star widget

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -6,20 +6,29 @@ import style from './tabs.css'
 
 /* eslint-disable */
 const Tabs = ({ tabs, dispatch }) => (
-  <ul className={style.tabs}>
-    {tabs.map(tab => (
-      <Tab
-        key={tab.id}
-        {...pick(tab, ['id', 'url', 'title', 'isActive'])}
-        select={() => dispatch(selectTab(tab.id))}
-        close={(event) => {
-          event.stopPropagation()
-          return dispatch(closeTab(tab.id))
-        }}
-      />
-    ))}
-    <li onClick={() => dispatch(addTab())} className={style.add} />
-  </ul>
+  <div className={style.container}>
+    <ul className={style.tabs}>
+      {tabs.map(tab => (
+        <Tab
+          key={tab.id}
+          {...pick(tab, ['id', 'url', 'title', 'isActive'])}
+          select={() => dispatch(selectTab(tab.id))}
+          close={(event) => {
+            event.stopPropagation()
+            return dispatch(closeTab(tab.id))
+          }}
+        />
+      ))}
+    </ul>
+    <div onClick={() => dispatch(addTab())} className={style.add} />
+    <iframe
+      src="https://ghbtns.com/github-btn.html?user=pd4d10&repo=chrome-ui&type=star"
+      frameBorder="0"
+      scrolling="0"
+      width="53px"
+      height="20px"
+    />
+  </div>
 )
 
 Tabs.propTypes = {
