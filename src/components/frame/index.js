44d64e870fd1a5829f365d09eb140e5e85941ad6
pd4d10 <pd4d10@gmail.com>
2017-01-11 23:52:14 +0800

    Fix frame src

--- src/components/frame/index.js
+++ src/components/frame/index.js
@@ -8,21 +8,14 @@ const handleLoad = ({ dispatch, id }) => () => dispatch(loadSuccess(id))
 
 const Frame = ({ tabs, dispatch }) => (
   <div className={style.content}>
-    {tabs.map(({ id, url, isActive, isFailed }) => (isFailed ? (
-      <div
-        key={id}
-        className={classNames(style.fail, {
-          [style.hidden]: !isActive,
-        })}
-      >fail</div>
-    ) : (
+    {tabs.map(({ id, url, isActive }) => (
       <iframe
         className={classNames({ [style.hidden]: !isActive })}
-        src={completeUrl(url)}
+        src={url ? completeUrl(url) : null}
         key={id}
         onLoad={handleLoad({ dispatch, id })}
       />
-    )))}
+    ))}
   </div>
 )
 
