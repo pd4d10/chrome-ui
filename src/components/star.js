e8a80836147a59d45a2ecfe6592c29e6efa8e4e9
pd4d10 <pd4d10@gmail.com>
2017-02-17 20:37:11 +0800

    Add icon links

--- src/components/star.js
+++ src/components/star.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react'
 
+// https://cs.chromium.org/chromium/src/components/toolbar/vector_icons/star.icon
 const Star = () => (
   <svg viewBox="0 0 16 16">
     <path d="M 8 4 L 9.12 6.79 L 12 7.05 L 9.82 9.04 L 10.47 12 L 8 10.43 L 5.53 12 L 6.18 9.04 L 4 7.05 L 6.88 6.79 L 8 4 M 8 0.5 L 5.89 5.56 L 0.5 6.03 L 4.59 9.64 L 3.37 15 L 8 12.16 L 12.64 15 L 11.41 9.64 L 15.5 6.03 L 10.11 5.56 L 8 0.5 Z"></path>
