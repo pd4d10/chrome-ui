c4e7c5ecd5b23aac8c8d5a0e34e4f87b36dcfc59
pd4d10 <pd4d10@gmail.com>
2017-03-20 23:29:27 +0800

    Fix style of tab close button

--- src/components/tab-close.js
+++ src/components/tab-close.js
@@ -3,13 +3,14 @@ import React from 'react'
 import style from './tab-close.css'
 
 const TabClose = props => (
-  <svg
-    viewBox="0 0 24 24"
-    className={style.close}
-    {...props}
-  >
-    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-  </svg>
+  <div className={style.close}>
+    <svg
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+    </svg>
+  </div>
 )
 
 export default TabClose
