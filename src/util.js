a09c00deb0d06f19c5bbba8bf2027aa97f15325a
pd4d10 <pd4d10@gmail.com>
2017-07-02 13:57:18 +0800

    Fix GitHub button

--- src/util.js
+++ src/util.js
@@ -44,5 +44,5 @@ export function getTitle(url) {
 
 // Github start widget
 export function getGithubWidget() {
-  return '<iframe allowtransparency="true" scrolling="no" frameborder="0" src="https://buttons.github.io/buttons.html#href=https%3A%2F%2Fgithub.com%2Fpd4d10%2Fchrome-ui&amp;text=Star&amp;data-count-api=%2Frepos%2Fpd4d10%2Fchrome-ui%23stargazers_count&amp;data-count-href=%2Fpd4d10%2Fchrome-ui%2Fstargazers&amp;data-count-aria-label=%23%20stargazers%20on%20GitHub&amp;data-style=&amp;data-icon=octicon-star&amp;aria-label=Star%20pd4d10%2Fchrome-ui%20on%20GitHub" style="width: 85px; height: 20px; border: none;"></iframe>'
+  return '<iframe allowtransparency="true" scrolling="no" frameborder="0" src="https://buttons.github.io/buttons.html#href=https%3A%2F%2Fgithub.com%2Fpd4d10%2Fchrome-ui&aria-label=Star%20pd4d10%2Fchrome-ui%20on%20GitHub&data-icon=octicon-star&data-text=Star&data-show-count=true" style="width: 85px; height: 20px; border: none;"><iframe/>'
 }
